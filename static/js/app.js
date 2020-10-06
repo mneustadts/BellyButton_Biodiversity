2ef840574ad14738789fd4fcf4123f80c86bff82
Michael Neustadter <mneustadts@outlook.com>
2020-10-06 13:22:26 -0500

    reupload

---
+++ static/js/app.js
@@ -0,0 +1,30 @@
+function findPlots(id) {
+    // read samples.json
+    d3.json('samples.json').then ( sampledata =>{
+        console.log(sampledata)
+        var ids = sampledata.samples[0].otuIds;
+        console.log(ids)
+        var sampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
+        console.log(sampleValues)
+        var labels = sampledata.samples[0].otuLabels.slice(0,10);
+        console.log(labels)
+
+    // get top ten otu ids for the otu plot
+        var otuTopTen = (sampledata.samples[0].otuIds.slice(0,10)).reverse();
+    
+    // get the correct form of otu ids to plot
+        var otuID = otuTopTen.map(d => 'OTU' + d);
+        console.log(`OTU IDS: ${otuID}`)
+    })
+
+
+
+
+
+
+
+
+
+
+
+}
