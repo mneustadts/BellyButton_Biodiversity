91d9dd23d08e320681343d45753f8c541af9c004
Michael Neustadter <mneustadts@outlook.com>
2020-10-06 16:00:37 -0500

    apps

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,12 @@
 function findPlots(id) {
     // read samples.json
-    d3.json('samples.json').then(sampledata =>{
+    d3.json('samples.json').then((sampledata) =>{
         console.log(sampledata)
-        var ids = sampledata.samples[0].otu_ids;
-        console.log(ids)
+        
         var sampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
-        console.log(sampleValues)
+        
         var labels = sampledata.samples[0].otu_labels.slice(0,10);
-        console.log(labels)
+        
 
     // get top ten otu ids for the otu plot
         var otuTopTen = (sampledata.samples[0].otu_ids.slice(0,10)).reverse();
@@ -18,7 +17,7 @@ function findPlots(id) {
     
     // top ten labels for the chart
         var labels = sampledata.samples[0].otu_labels.slice(0,10);
-        console.log(`OTU_labels: ${labels}`)
+       
         var barTrace = {
             x: sampleValues,
             y: OTU_id,
@@ -70,19 +69,53 @@ function findPlots(id) {
     var bubbleData = [bubbleTrace];
 
     Plotly.newPlot('bubble', bubbleData, bubbleLayout)
-
-    //function for necessary data
-    function findInfo(id){}
-    })
-
-
-
-
-
-
-
-
-
-
-
+    });
 }
+    //function for necessary data
+    function findInfo(id){
+    d3.json("samples.json").then((sampledata)=> {
+        
+    // get the metadata info for the demographic panel
+        var metadata = sampledata.metadata;
+        
+    // filter meta data info by id
+        var result = metadata.filter(meta => meta.id.toString() === id)[0];
+
+    // select demographic panel to put data and empty the demographic info
+        var demographicInfo = d3.select("#sample-metadata");
+        
+        demographicInfo.html("");
+
+    // get the necessary demographic data data for the id
+        Object.entries(result).forEach((key) => {   
+                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+        });
+    });
+}
+
+// create the function for the change event
+function optionChanged(id) {
+    findPlot(id);
+    findInfo(id);
+}
+
+// create the function for the initial data rendering
+function init() {
+    var dropdown = d3.select("#selDataset");
+
+    // read the data 
+    d3.json("samples.json").then((sampledata)=> {
+        console.log(data)
+
+    // get the id data to the dropdwown menu
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+
+    // call the functions to display plots on page
+        findPlot(sampledata.names[0]);
+        findInfo(sampledata.names[0]);
+    });
+}
+
+init();
